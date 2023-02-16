e4ea9656e1d35a3786a5fb0611b679fdbf13d30e
Helgi <helgiskafta@gmail.com>
2023-02-16 22:18:00 +0100

    Added a few helpful enum extensions to the mix.

---
+++ Scripts/ExtensionMethods/EnumExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace hrolgarUllr.ExtensionMethods
+{
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Returns the opposite value of a two-option enum.
+        /// </summary>
+        /// <typeparam name="TEnum">The enum type.</typeparam>
+        /// <param name="value">The value to get the opposite of.</param>
+        /// <returns>The opposite value of the enum.</returns>
+        public static TEnum GetOpposite<TEnum>(this TEnum value) where TEnum : struct, Enum
+        {
+            if(!typeof(TEnum).IsEnum)
+                throw new ArgumentException($"{typeof(TEnum)} is not an enum type.");
+            
+            var values = Enum.GetValues(typeof(TEnum));
+            if(values.Length != 2)
+                throw new ArgumentException($"{typeof(TEnum)} does not have exactly 2 values.");
+            
+            var first = values.GetValue(0);
+            var second = values.GetValue(1);
+            return value.Equals(first) ? (TEnum)second : (TEnum)first;
+        }
+        
+        /// <summary>
+        /// Returns a random value from an enum.
+        /// </summary>
+        /// <typeparam name="TEnum">The enum type.</typeparam>
+        /// <returns>A random value from the enum.</returns>
+        public static TEnum GetRandomValue<TEnum>(this TEnum value) where TEnum : struct, Enum
+        {
+            if(!typeof(TEnum).IsEnum)
+                throw new ArgumentException($"{typeof(TEnum)} is not an enum type.");
+
+            var values = Enum.GetValues(typeof(TEnum));
+            return (TEnum)values.GetValue(UnityEngine.Random.Range(0, values.Length));
+        }
+    }
+}
